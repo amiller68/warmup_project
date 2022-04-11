decc771b36f84da990e456d50d7c2a0ad05ef34f
alex <alexanderscottmiller@gmail.com>
2022-04-10 23:57:15 -0500

    Finished code, got a rosbag of the wall follower

--- scripts/follow_wall.py
+++ scripts/follow_wall.py
@@ -4,6 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+
 class WallFollower():
     def __init__(self):
         rospy.init_node("wall_follower")
@@ -22,24 +23,45 @@ class WallFollower():
         # A class variable to hold our scan state
         self.scanState = None
 
-        # The fastest our robot should turn in order to follow a wall
-        self.max_angular_vel = 1.0
-        # The fastest our robot should move along a wall
-        self.max_linear_vel = .75
-        # The closest the robot should get to the wall
-        self.min_dist = 0.25
-        # Farthest the robot should be from a wall
-        self.max_dist = 0.5
-        self.max_scan = 4.0
+        # The base speed our robot should turn in order to follow a wall
+        self.base_angular_vel = 2.5
+        # A base speed for our robot to move at to follow a wall
+        self.base_linear_vel = .075
 
+        # The closest the robot should get to the wall
+        self.wall_dist = .25
+        # The point at which the robot should start turning away from a wall in front of it
+        self.wall_warn = .5
+
+        # Initialize variables to hold our Scan state
+
+        # All the points around the robot
+        self.scanState = None
+        # The farthest our robot can scan
+        self.max_scan_dist = None
+        # A distance reading to warm us if we're going to run into a wall
+        self.warn_distance = None
 
     # Update our state of data ranges from the Subscriber
     def updateScanState(self, msg):
+        if not self.scanState:
+            self.max_scan_dist = msg.range_max
         # Extract the observed ranges and save them into state
         self.scanState = msg.ranges
 
+        # If we get a valid reading right in front of us
+        if self.scanState[0]:
+            # Set it
+            self.warn_distance = self.scanState[0]
+        else:
+            # Otherwise, it means the point is too far away
+            self.warn_distance = self.max_scan_dist
+
+    # Find the object closest to the robot
     def closestPoint(self):
+        # Extract the ranges our lidar sensor recorded
         ranges = self.scanState
+        # Initialize a variable to hold our target
         closest_point = None
         # Iterate through the ranges stored in our scan state, keeping track of range degrees
         for i in range(360):  # Scan through all 360 degrees of data
@@ -56,67 +78,65 @@ class WallFollower():
                 closest_point = ranges[i], i
         return closest_point
 
-    def findWall(self):
-        if self.min_dist <= self.scanState[90] <= self.max_dist:
-            return
-        movement = Twist()
-        cp = self.closestPoint()
-        movement.linear.x = self.max_linear_vel
-        # If there's nothing nearby, go find something
-        print("Moving towards wall")
-        while (not cp or cp[0] > self.max_dist) and not rospy.is_shutdown():
-            if cp:
-                r = cp[0]
-                movement.linear.x = (r - self.max_dist) / (self.max_scan - self.max_dist) * self.max_linear_vel + .05
-            self.move.publish(movement)
-            cp = self.closestPoint()
-            self.rateLimit.sleep()
-
-        movement = Twist()
-        horizontal_dist = self.scanState[90]
-        print("Orienting towards wall")
-        while horizontal_dist == 0 and not rospy.is_shutdown():
-            movement.angular.z = - self.max_angular_vel
-            horizontal_dist = self.scanState[90]
-            self.move.publish(movement)
-            self.rateLimit.sleep()
-
-        print("Found a distance")
-        while not (horizontal_dist < self.max_dist) and not rospy.is_shutdown():
-            movement.angular.z = - (horizontal_dist - self.max_dist) / (self.max_scan - self.max_dist) * self.max_angular_vel - .1
-            print("Turning at: ", movement.angular.z)
-            horizontal_dist = self.scanState[90]
-            self.move.publish(movement)
-            self.rateLimit.sleep()
-        movement = Twist()
-        self.move.publish(movement)
-
-    # follow a wall given our current scan state
-    # Publishes a velocity that orients the bot towards the wall
-    # Keeps walls on the robots left
+    # Follow a wall on the robots left based on the closest point to the robot
+    # This method determines a linear and angular pid to scale the base speeds
+    # And then published the scaled velocity to the cmd_vel topic
     def followWall(self):
-        # Extract the ranges our lidar sensor recorded
-        ranges = self.scanState
+        # Determine the closet point
+        cp = self.closestPoint()
+        linear_pid = 1
+        angular_pid = 1
 
-        # Use the measurements 45 deg and 135 deg left of center in order to orient the robot
-        range_45_deg = ranges[45]
-        range_135_deg = ranges[135]
+        # In case there's not, go and find one
+        if cp:
+            # Extract its range and angle
+            r, a = cp
 
-        # Orient the robot to follow a wall at any distance
+            # Determine our angular pid control scalar
 
+            # If the point is on the robots left
+            if 0 <= a < 180:
+                # Make the robot turn towards the wall
+                angular_pid = (a - 90) / 90
+            # Same if the robot is on the right
+            else:
+                angular_pid = -(a - 270) / 90
+
+            # This term is used to accelerate the turns and decelerate the linear movement of the bot when needed
+            # If the robot gets two far away we want to be able to recover
+            distance_penalty = (r - self.wall_dist) / (self.max_scan_dist - self.wall_dist)
+
+            # Increase the amplitude of our linear velocity accordingly
+            if angular_pid > 0:
+                angular_pid = angular_pid + distance_penalty
+            else:
+                angular_pid = angular_pid - distance_penalty
+
+            # Determine Linear PID, and affect the angular pid based on special cases
+
+            # If the robot is approaching a wall
+            if self.warn_distance <= self.wall_warn:
+                # Slow down as we approach
+                linear_pid = pow(((self.warn_distance - self.wall_dist) / (self.wall_warn - self.wall_dist)), 2)
+                # Turn faster as we approach
+                angular_pid = -(1 - linear_pid)
+
+            # Otherwise, if we're getting too far away from the wall
+            elif self.wall_dist < r:
+                # Move Slower the faster we're turning,
+                # and subtract a penalty based on how far away we are from the wall
+                linear_pid = (1 - abs(angular_pid)) - distance_penalty
+                # Turn into the wall faster
+                angular_pid = angular_pid + distance_penalty
+
+            # If we're within range, merely move slower the faster we're turning
+            else:
+                linear_pid = 1 - abs(angular_pid)
+
+        # Initialize a movement, calculate its velocity using our PIDs and publish it
         movement = Twist()
-        # If we're heading towards a wall
-        if abs(range_45_deg - range_135_deg) < .1:
-            movement.linear.x = self.max_linear_vel
-        elif range_45_deg < range_135_deg:
-            print("On a collision course")
-            # Take the difference of our measurements
-            diff = range_135_deg - range_45_deg
-            movement.angular.z = - self.max_angular_vel / 10
-        elif range_45_deg > range_135_deg:
-            print("Moving away from a wall")
-            diff = range_135_deg - range_45_deg
-            movement.angular.z = self.max_angular_vel / 10
+        movement.linear.x = linear_pid * self.base_linear_vel
+        movement.angular.z = angular_pid * self.base_angular_vel
         self.move.publish(movement)
 
     def shutdown(self):
@@ -129,13 +149,10 @@ class WallFollower():
         while self.scanState is None and not rospy.is_shutdown():
             pass
         print("Ready to follow!")
-
-        # print("Looking for a wall")
-        # self.findWall()
-
         while not rospy.is_shutdown():
             self.followWall()
             self.rateLimit.sleep()
+
 
 if __name__ == '__main__':
     try:
